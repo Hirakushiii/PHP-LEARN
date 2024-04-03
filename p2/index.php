4597fdcd566920f450586748d43c75b4b8adfede
Kenn <141384556+Hirakushiii@users.noreply.github.com>
2024-04-04 01:26:29 +0700

    first commit

---
+++ p2/index.php
@@ -0,0 +1,20 @@
+<?php
+$nama = "Kenn";
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>php p2</title>
+</head>
+
+<body>
+    <h1>hallo, nama saya
+        <?php echo $nama; ?>
+    </h1>
+</body>
+
+</html>
