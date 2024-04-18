e0f21a9dfbb917694db5bac6275cad7d0139a536
Kenn <141384556+Hirakushiii@users.noreply.github.com>
2024-04-18 23:29:21 +0700

    Update table.php

--- p9/table.php
+++ p9/table.php
@@ -18,7 +18,7 @@ $data = mysqli_query($connect, "SELECT * FROM saham");   //AMBIL DATA SAHAM PADA
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet"
         integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
-    <title>Document</title>
+    <title>DATA SAHAM IDX</title>
 </head>
 
 <body>
